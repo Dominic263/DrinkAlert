22b6ac8277512b000d268557fd545b6b6db8c8bd
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2022-08-28 03:54:11 -0400

    Initial Commit

---
+++ DrinkAlert/DrinkAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkAlertApp.swift
+//  DrinkAlert
+//
+//  Created by DOMINIC NDONDO on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct DrinkAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
